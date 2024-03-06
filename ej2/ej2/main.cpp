75b895d1f0b60997c2a0243a0ddc6ba356d12967
sosa-facundo <sosafacundo.94@gmail.com>
2024-03-05 23:01:12 -0300

    ejercio 2

---
+++ ej2/ej2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N1 , N2;
+    cout << "ingrese 2 numeros" << endl;
+    cin >> N1;
+    cin >> N2;
+
+    if (N1<N2)
+    {
+      cout << N1 << endl;
+    }
+    else
+    {
+       cout << N2 << endl;
+    }
+
+    return 0;
+}
